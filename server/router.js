445f77bdf252f3bba5a53caecd71669e89444654
e-seng <eth.sengsavang@gmail.com>
2022-04-11 16:37:59 -0600

    Ensure mysql connection from client is working
    note: a user with auth data as specified by `MYSQL_USER` and
    `MYSQL_PASSWORD` must be defined to accept connections from any host.
    
    ie: `CREATE USER "$MYSQL_USER"@"%" IDENTIFIED BY "$MYSQL_PASSWORD"`

--- server/router.js
+++ server/router.js
@@ -3,8 +3,36 @@ const mysql = require('mysql');
 const express = require('express');
 var router = express.Router();
 
+// get user login information for the database
+const sql_host = "cpsc471_database";
+const sql_user = process.env.MYSQL_USER || "root";
+const sql_pass = process.env.MYSQL_PASSWORD || "111111";
+
+function generateConnection(){
+  // connect to the database within network
+  var conn = mysql.createConnection({
+    host: sql_host,
+    user: sql_user,
+    password: sql_pass,
+  });
+
+  console.log(`[info] user "${sql_user}", pass "${sql_pass}"`);
+  return conn;
+}
+
 router.get("/", (request, response) => {
   response.send("seems to be working fine!");
+});
+
+router.get("/test", (request, response) => {
+  var conn = generateConnection();
+  conn.connect((err) => {
+    if(err){
+      response.send(err.message);
+      return;
+    }
+    response.send("still good");
+  });
 });
 
 module.exports = router;
