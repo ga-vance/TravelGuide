cac8b68a90224e952aa519c55952736b54726ab6
e-seng <eth.sengsavang@gmail.com>
2022-04-15 10:02:24 -0600

    Allow customers to reserve seats on a flight

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 // allow CORS for every request
 app.use(function(req, res, next) {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE, PUTS');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization');
   next();
 });
