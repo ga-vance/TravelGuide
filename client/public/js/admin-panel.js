10e5ccf221694400e188092f398a7aaeaf601e6b
e-seng <eth.sengsavang@gmail.com>
2022-04-15 12:20:23 -0600

    Update admin panel to show admin stuff and block unauthorized users

--- client/public/js/admin-panel.js
+++ client/public/js/admin-panel.js
@@ -5,6 +5,31 @@
 function admin(){
   const apiOrigin = `http://${window.location.hostname}:3000`;
 
+  var token = localStorage.getItem("sesstoken");
+  var tokenData;
+
+  try{
+    tokenData = JSON.parse(atob(token.split('.')[1]));
+    if(tokenData.failed){
+      console.error("[error] cannot access admin panel");
+      console.error(tokenJson.message);
+      window.location.replace("/index.html");
+      return;
+    }
+
+    if(!tokenData.isAdmin){
+      console.error("[error] cannot access admin panel");
+      console.error("logged in user is not an admin");
+      window.location.replace("/user-panel.html");
+      return;
+    }
+  }catch(err){
+    console.error("[error] cannot access admin panel");
+    console.error("nonexistent or malformed token");
+    window.location.replace("/index.html");
+    return;
+  }
+
   // specify the element to log out
   document.querySelector("#logout").addEventListener("click", () => {
     localStorage.removeItem("sesstoken");
